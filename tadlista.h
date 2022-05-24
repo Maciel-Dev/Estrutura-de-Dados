09a9653bd03c389eb915deb2f26a643c4c6d5c10
João Vitor Maciel Vianna <82848067+Maciel-Dev@users.noreply.github.com>
2022-05-24 09:07:56 -0300

    Lista.h em C
    Complemento para lista.c

---
+++ tadlista.h
@@ -0,0 +1,34 @@
+typedef void *tdado;
+
+typedef struct tnoh{
+    tdado dado;
+    struct tnoh *proximo;
+} tnode;
+
+typedef tnode* pnoh;
+
+typedef struct {
+    pnoh primeiro;
+    pnoh ultimo;
+    int tamanho;
+} tcabec;
+
+typedef tcabec* Lista;
+
+
+//***********************************************************************
+//*
+//* INTERFACE DO TAD LISTA
+//*
+//***********************************************************************
+
+Lista criaLista();
+Lista appendLista(Lista lst, tdado dado);
+int lenLista(Lista lst);
+tdado primLista(Lista lst);
+tdado ultLista(Lista lst);
+int indexLista(Lista lst, tdado dado);
+Lista clearLista(Lista lst);
+Lista insertLista(Lista lst, int pos, tdado dado);
+Lista removeLista(Lista lst, int pos);
+
